246bef7fd3e0ca7a36d67555baa806cddfd7af58
Shana12345 <shana_charlery@msn.com>
2020-03-17 13:44:06 +0000

    quick changes to installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -2,8 +2,10 @@
 source venv/bin/activate
 pip3 install Flask
 pip3 install flask_mysqldb
-source ~/bashrc
 python3 app.py
+python3 -m pip install pytest
+python3 -m pip install urllib3
+python3 -m pip install pytest-base-url
+
+source ~/.bashrc
 python3 /var/lib/jenkins/workspace/Pipeline1/app.py
-python -m pip install pytest
-pip3 install urllib3
