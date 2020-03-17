4f70a15288753fbd97388677c26459732da8619b
Shana12345 <shana_charlery@msn.com>
2020-03-17 13:44:29 +0000

    quick changes to installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -7,5 +7,5 @@ python3 -m pip install pytest
 python3 -m pip install urllib3
 python3 -m pip install pytest-base-url
 
-source ~/.bashrc
+source ~/bashrc
 python3 /var/lib/jenkins/workspace/Pipeline1/app.py
