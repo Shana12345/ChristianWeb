89ddcc0bfe6372c566c1efa3200ed6631a9e3f59
Shana12345 <shana_charlery@msn.com>
2020-03-17 13:55:00 +0000

    quick changes to installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -2,7 +2,6 @@
 source venv/bin/activate
 pip3 install Flask
 pip3 install flask_mysqldb
-python3 app.py
 python3 -m pip install pytest
 python3 -m pip install urllib3
 python3 -m pip install pytest-base-url
