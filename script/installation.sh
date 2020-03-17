499ff001f6cd968d6dae6799732c4dfa4f9438d1
Shana12345 <shana_charlery@msn.com>
2020-03-17 12:20:30 +0000

    update for jenkins

--- script/installation.sh
+++ script/installation.sh
@@ -4,3 +4,6 @@ pip3 install Flask
 pip3 install flask_mysqldb
 source ~/bashrc
 python3 app.py
+python3 /var/lib/jenkins/workspace/Pipeline1/app.py
+python -m pip install pytest
+pip3 install urllib3
