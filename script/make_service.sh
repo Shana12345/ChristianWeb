b4bd4e0518c69dc8753f9fc05a2f8965cb457e68
Shana12345 <shana_charlery@msn.com>
2020-03-21 22:29:17 +0000

    update to test DB

---
+++ script/make_service.sh
@@ -0,0 +1,11 @@
+udo cp /var/lib/jenkins/workspace/Pipeline1/flask.service /etc/systemd/system
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable flask.service
+
+
+
+sudo systemctl stop flask.service
+
+sudo systemctl start flask.service
