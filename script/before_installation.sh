142bb46d50f354d548970f67f143cdd95e2ee812
Shana12345 <shana_charlery@msn.com>
2020-03-21 23:09:22 +0000

    another testing

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo cp /var/lib/jenkins/workspace/Pipeline1/script/run.sh /etc/systemd/system
+sudo cp /var/lib/jenkins/workspace/Pipeline1/script/flask.service /etc/systemd/system
 sudo apt update -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 source ~/bashrc
-sudo cp /var/lib/jenkins/workspace/Pipeline1/flask.service /etc/systemd/system
 python3 /var/lib/jenkins/workspace/Pipeline1/app.py
 
