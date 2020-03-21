86a3e65877bf9570917eedb3a96d8c8a24053d4e
Shana12345 <shana_charlery@msn.com>
2020-03-21 22:43:47 +0000

    again

--- script/make_service.sh
+++ script/make_service.sh
@@ -1,4 +1,5 @@
 sudo cp /var/lib/jenkins/workspace/Pipeline1/flask.service /etc/systemd/system
+sudo cp /var/lib/jenkins/workspace/Pipeline1/script/run.sh /etc/systemd/system
 sudo systemctl daemon-reload
 
 sudo systemctl enable flask.service
