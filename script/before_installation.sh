4299c45ed281c6a0f484d580d2a23fb48bd35bc1
Shana12345 <shana_charlery@msn.com>
2020-03-21 23:20:54 +0000

    final of testing

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -6,4 +6,4 @@ sudo apt install python3-venv -y
 sudo apt-get install libmysqlclient-dev -y
 python3 -m venv venv
 sudo systemctl daemon-reload
-sudo cp /var/lib/jenkins/workspace/Pipeline1/script/flask.service/run.sh /etc/systemd/system
+sudo cp /var/lib/jenkins/workspace/Pipeline1/script/run.sh /etc/systemd/system
