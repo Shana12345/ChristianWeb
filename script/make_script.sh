04c33363272078e167475c68be0f3b883ad6aaca
Shana12345 <shana_charlery@msn.com>
2020-03-22 01:36:29 +0000

    otravez

--- script/make_script.sh
+++ script/make_script.sh
@@ -3,5 +3,5 @@
 source /var/lib/jenkins/bashrc
 source /var/lib/jenkins/workspace/Pipeline1/venv/bin/activate
 
-/var/lib/jenkins/workspace/Pipeline1/venv/bin/coverage run -m pytest /var/lib/jenkins/workspace/Pipeline1/test/testing.py
-/var/lib/jenkins/workspace/Pipeline1/venv/bin/coverage report -m
+/var/lib/jenkins/workspace/Pipeline1/coverage run -m pytest /var/lib/jenkins/workspace/Pipeline1/test/testing.py
+/var/lib/jenkins/workspace/Pipeline1/coverage
