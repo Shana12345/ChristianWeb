6a9f2484edb253158511978421792395fe7be067
Shana12345 <shana_charlery@msn.com>
2020-03-22 01:24:19 +0000

    againnn

--- script/make_script.sh
+++ script/make_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /var/lib/jenkins/bashrc
-source /var/lib/jenkins/workspace/Pipeline/venv/bin/activate
+source /var/lib/jenkins/workspace/Pipeline1/venv/bin/activate
 
 /var/lib/jenkins/workspace/Pipeline1/venv/bin/coverage run -m pytest /var/lib/jenkins/workspace/Pipeline1/test/testing.py
 /var/lib/jenkins/workspace/Pipeline1/venv/bin/coverage report -m
--- script/make_serivce.sh
+++ script/make_serivce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp /var/lib/jenkins/workspace/Pipeline/flask.service /etc/systemd/system
+sudo cp /var/lib/jenkins/workspace/Pipeline1/flask.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable flask.service
 
